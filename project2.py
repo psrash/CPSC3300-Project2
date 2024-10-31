49a027dbb39bad1b04c8903733c276783aa4942a
sbcrook7 <sbcrook@g.clemson.edu>
2024-10-30 22:01:23 -0400

    testing bit manipulation

--- project2.py
+++ project2.py
@@ -2,8 +2,18 @@ import sys
 
 def readInFile(filename):
     in_file = open(filename, "rb")
-    op = in_file.read(6)
-    print(op)
+    raw_data = in_file.read(4)
+    instruction = int.from_bytes(raw_data, byteorder='big')
+
+    for i in range(31, -1, -1):
+        bit = (instruction >> i) & 1
+        print(bit, end='')
+
+    op = (instruction >> 26) & 0b111111
+    print(f"The operand is: {op:06b}")
+
+    rs_register = (instruction >> 21) & 0b11111
+    print(f"The rs register is: {rs_register:05b}")
 
 def main():
     filename = sys.argv[1]
