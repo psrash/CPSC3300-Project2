c9705a0ce3f83fcd2347e94542224e6b8cf43bc6
phoenixsrash <psrash19@gmail.com>
2024-10-30 21:19:51 -0400

    adding output file
    Co-authored-by: sbcrook7 <sbcrook7@users.noreply.github.com>

--- project2.py
+++ project2.py
@@ -1,10 +1,13 @@
-# hello
-#test 
-#tes test test
+import sys
 
+def readInFile(filename):
+    in_file = open(filename, "rb")
+    op = in_file.read(6)
+    print(op)
 
 def main():
-    print("hello")
+    filename = sys.argv[1]
+    readInFile(filename)
 
 if __name__ == "__main__":
     main()
